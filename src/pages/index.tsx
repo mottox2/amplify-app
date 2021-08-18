ed99df9cadb24a4e7c2b13a63c93571011328c59
mottox2 <y.mottox2@gmail.com>
2021-08-18 19:39:04 +0900

    Add minimal amplify example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { API } from "aws-amplify";
+import { listBlogs } from "../graphql/queries";
+import { useEffect } from "react";
+import { createBlog } from "../graphql/mutations";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    API.graphql({
+      query: listBlogs,
+    });
+  });
+
+  const onClick = () => {
+    API.graphql({
+      query: createBlog,
+      variables: {
+        input: {
+          name: "Hello",
+        },
+      },
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,13 +33,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <button onClick={onClick}>Create</button>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -59,14 +82,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
